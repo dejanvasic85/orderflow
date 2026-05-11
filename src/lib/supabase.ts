daeb0c5026e6aaa2e18c6720a47a4d337b03325b
Dejan Vasic <dejanvasic24@gmail.com>
2026-05-11 21:38:52 +1000

    feat: improving the domain and db structure

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,8 @@
 import { createBrowserClient } from "@supabase/ssr";
 
-export const supabase = createBrowserClient(
+import type { Database } from "@/lib/database.types";
+
+export const supabase = createBrowserClient<Database>(
   import.meta.env.VITE_SUPABASE_URL,
   import.meta.env.VITE_SUPABASE_ANON_KEY,
 );
