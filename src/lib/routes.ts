f888dfb798ba382215679fdb606178effbad40ee
Dejan Vasic <dejanvasic24@gmail.com>
2026-05-30 21:55:15 +1000

    feat: feature to browse catalog items

--- src/lib/routes.ts
+++ src/lib/routes.ts
@@ -1,13 +1,8 @@
-import { BookOpenIcon, BuildingIcon, ShoppingCartIcon, UsersIcon, HomeIcon } from "lucide-react";
+import { BuildingIcon, ShoppingCartIcon, UsersIcon, HomeIcon } from "lucide-react";
 
 export const adminNavItemsValue = [
   { label: "Home", to: "/manage/dashboard", icon: HomeIcon },
   { label: "Orders", to: "/manage/orders", icon: ShoppingCartIcon },
   { label: "Accounts", to: "/manage/accounts", icon: BuildingIcon },
   { label: "Users", to: "/manage/users", icon: UsersIcon },
-] as const;
-
-export const accountNavItemsValue = [
-  { label: "Orders", to: "/accounts", icon: ShoppingCartIcon },
-  { label: "Browse", to: "/browse", icon: BookOpenIcon },
 ] as const;
